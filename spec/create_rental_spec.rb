ef0d329329e95cce935410427588d2c99c891384
jokelly106 <johnokelly106@gmail.com>
2020-10-07 12:05:59 -0400

    add update rental tests
    Adds tests for updating a rental. Also removes a global variable in the create rental test.

--- spec/create_rental_spec.rb
+++ spec/create_rental_spec.rb
@@ -1,56 +1,61 @@
 require 'rails_helper'
 
-$rental = FactoryBot.create(:rental)
+
 describe "create a rental", :type => :request do
+    rental = FactoryBot.create(:rental)
     before do
         post '/rentals', params: { :rental => { 
-            :title => $rental.title,
-            :owner => $rental.owner,
-            :city => $rental.city,
-            :lat => $rental.lat,
-            :lng => $rental.lng,
-            :image => $rental.image,
-            :bedrooms => $rental.bedrooms,
-            :description => $rental.description,
-            :category => $rental.category
+            :title => rental.title,
+            :owner => rental.owner,
+            :city => rental.city,
+            :lat => rental.lat,
+            :lng => rental.lng,
+            :image => rental.image,
+            :bedrooms => rental.bedrooms,
+            :description => rental.description,
+            :category => rental.category
             }
          }
     end
 
     it 'returns the rental title' do
-        expect(JSON.parse(response.body)['data']['attributes']['title']).to eq($rental.title)
+        expect(JSON.parse(response.body)['data']['attributes']['title']).to eq(rental.title)
     end
 
     it 'returns the rental owner' do
-        expect(JSON.parse(response.body)['data']['attributes']['owner']).to eq($rental.owner)
+        expect(JSON.parse(response.body)['data']['attributes']['owner']).to eq(rental.owner)
     end
 
     it 'returns the rental city' do
-        expect(JSON.parse(response.body)['data']['attributes']['city']).to eq($rental.city)
+        expect(JSON.parse(response.body)['data']['attributes']['city']).to eq(rental.city)
     end
 
     #For lat/lng, remember the serializer puts it into a location object for the web app.
     it 'returns the rental lat' do
-        expect(JSON.parse(response.body)['data']['attributes']['location']['lat']).to eq($rental.lat)
+        expect(JSON.parse(response.body)['data']['attributes']['location']['lat']).to eq(rental.lat)
     end
 
     it 'returns the rental lng' do
-        expect(JSON.parse(response.body)['data']['attributes']['location']['lng']).to eq($rental.lng)
+        expect(JSON.parse(response.body)['data']['attributes']['location']['lng']).to eq(rental.lng)
     end
 
     it 'returns the rental image' do
-        expect(JSON.parse(response.body)['data']['attributes']['image']).to eq($rental.image)
+        expect(JSON.parse(response.body)['data']['attributes']['image']).to eq(rental.image)
     end
 
     it 'returns the rental bedrooms' do
-        expect(JSON.parse(response.body)['data']['attributes']['bedrooms']).to eq($rental.bedrooms)
+        expect(JSON.parse(response.body)['data']['attributes']['bedrooms']).to eq(rental.bedrooms)
     end
 
     it 'returns the rental description' do
-        expect(JSON.parse(response.body)['data']['attributes']['description']).to eq($rental.description)
+        expect(JSON.parse(response.body)['data']['attributes']['description']).to eq(rental.description)
     end
 
     it 'returns the rental category' do
-        expect(JSON.parse(response.body)['data']['attributes']['category']).to eq($rental.category)
+        expect(JSON.parse(response.body)['data']['attributes']['category']).to eq(rental.category)
+    end
+
+    it 'returns a created status' do
+        expect(response).to have_http_status(:created)
     end
 end
